952d683da9ef3b5c01e951a071b06487813d7daa
tarrinneal <tarrinneal@gmail.com>
2021-02-21 21:06:17 -0800

    added a little border

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Stories = ({stories, pickStory}) => {
   let stores = stories.map(story => {
-    return (<div key={story.storyTitle} id={story._id} className={story.storyTitle} id={story._id} onClick={pickStory}>
+    return (<div key={story.storyTitle} id={story._id} className={story.storyTitle + ' listItem'} id={story._id} onClick={pickStory}>
       <div className={story.storyTitle} id={story._id}>{story.author}</div>
       <div className={story.storyTitle} id={story._id}>{story.storyTitle}</div>
     </div>)
